3b2fa16658b3f3ceec035db1da042bcb7bb22f74
Arpit Surana <arpit@permissionles.org>
2022-11-07 15:17:51 +0530

    Partner section done

--- src/Components/Partner/Partner.jsx
+++ src/Components/Partner/Partner.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import "./Partner.css";
+import { PartnerData } from "./PartnerData";
 const Partner = () => {
+  const [data] = useState(PartnerData);
   return (
     <>
       <section className="partner-section">
         <div className="partner-container">
-          <div className="partner-wrapper"></div>
+          <div className="partner-wrapper">
+            {data.map((el) => {
+              return (
+                <div key={el.id}>
+                  <img src={el.image} />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
---
+++ src/Components/Partner/PartnerData.js
@@ -0,0 +1,52 @@
+export const PartnerData = [
+  {
+    id: "1",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/63091495887e0238ea5b9e79_logo_clear.svg",
+  },
+  {
+    id: "2",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/6309149f64062049c14c30ba_logo_one.svg",
+  },
+  {
+    id: "3",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/6286adb1a1301e6f7cc5b591_TED%20logo.svg",
+  },
+  {
+    id: "4",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/6000971fd4b15ea72a9f68c9_praxis-logo.svg",
+  },
+  {
+    id: "5",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/630914b26130727e43760833_logo_iex.svg",
+  },
+  {
+    id: "6",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/6000971fd4b15e79be9f6754_legacybox.svg",
+  },
+  {
+    id: "7",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/6000971fd4b15e096e9f6419_hrw.svg",
+  },
+  {
+    id: "8",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/63091553e00e874c9b552c0c_logo_trilith.svg",
+  },
+  {
+    id: "9",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/630914d586772310d9599ed1_logo_community.svg",
+  },
+  {
+    id: "10",
+    image:
+      "https://assets-global.website-files.com/6000971fd4b15ea4ee9f6405/6000971fd4b15e062d9f6750_ncr.svg",
+  },
+];
