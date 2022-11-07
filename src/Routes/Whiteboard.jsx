f37a7b94eb304935f3134aa1472b827d46f63240
Arpit Surana <arpit@permissionles.org>
2022-11-07 13:33:33 +0530

    folder structure done

--- src/Routes/Whiteboard.jsx
+++ src/Routes/Whiteboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "../Components/LandingPage";
+import LandingPage from "../Pages/LandingPage";
 const Whiteboard = () => {
   return (
     <div>
